3bd888674141bee0d717029ac11409f265439a89
Arian <arian.kulmer@web.de>
2024-05-31 01:48:42 +0200

    nuwa: Enable single_tap sensor and set permissions for touch sensors
    Change-Id: I777ed71ecc5dc502182567bc7e1166605251af93

--- device.mk
+++ device.mk
@@ -10,6 +10,10 @@ $(call inherit-product, device/xiaomi/sm8550-common/common.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/nuwa/nuwa-vendor.mk)
 
+# Init
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init/init.nuwa.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.nuwa.rc \
+
 # Overlay
 PRODUCT_PACKAGES += \
     SettingsProviderResNuwa \
