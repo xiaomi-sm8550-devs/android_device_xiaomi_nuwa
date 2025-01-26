31a35b0fb51b5c7f1a20490f14abbc832b51d7ac
SkyMinus <minus_rav@qq.com>
2025-01-26 20:40:02 +0800

    nuwa: Set screen density to 560
    Change-Id: I150eb863d6c88d98ff0f20e086f8ec3c895a454c
    Signed-off-by: SkyMinus <minus_rav@qq.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ DEVICE_PATH := device/xiaomi/nuwa
 # Inherit from xiaomi sm8550-common
 include device/xiaomi/sm8550-common/BoardConfigCommon.mk
 
+# Display
+TARGET_SCREEN_DENSITY := 560
+
 # Inherit from the proprietary version
 include vendor/xiaomi/nuwa/BoardConfigVendor.mk
 
