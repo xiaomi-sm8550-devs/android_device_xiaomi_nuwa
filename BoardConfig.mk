c80f3c6f95b02372afce75a9a23980329e1eee28
SkyMinus <minus_rav@qq.com>
2025-02-02 20:27:57 +0800

    nuwa: Load fts_touch_spi.ko in recovery * Fix touch in recovery mode
    Change-Id: I3c94ad7bcb5931173a3cb0c440311bd00b1007da
    Signed-off-by: SkyMinus <minus_rav@qq.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,9 +18,12 @@ BOARD_VENDOR_KERNEL_MODULES_LOAD += \
 	goodix_fod.ko \
 	fts_touch_spi.ko
 
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += \
+	fts_touch_spi.ko
+
 BOOT_KERNEL_MODULES += \
 	fts_touch_spi.ko
-	
+
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := nuwa
 
