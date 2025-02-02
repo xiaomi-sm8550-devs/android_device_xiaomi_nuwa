a0ef2930c0bcc396fd91df01f9d4791be6706dcf
SkyMinus <minus_rav@qq.com>
2025-02-02 19:55:15 +0800

    nuwa: Add missing OTA assert
    Change-Id: Id59a1e9cd211143ccc54e0c71c9f46f277042e7e
    Signed-off-by: SkyMinus <minus_rav@qq.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,9 @@ BOARD_VENDOR_KERNEL_MODULES_LOAD += \
 BOOT_KERNEL_MODULES += \
 	fts_touch_spi.ko
 	
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := nuwa
+
 # Powershare
 TARGET_POWERSHARE_PATH := /sys/class/qcom-battery/reverse_chg_mode
 
