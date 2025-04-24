15e53a4c12bf9ec1ece7aedae0a6b6126d60d11f
Bruno Martins <bgcngm@gmail.com>
2025-04-24 21:36:24 +0100

    nuwa: Switch to PowerShare AIDL
    Change-Id: I97400638493a3b9d5a3b87fa6831c9affc7c3def
    Signed-off-by: SkyMinus <minus_rav@qq.com>

--- device.mk
+++ device.mk
@@ -39,7 +39,7 @@ PRODUCT_COPY_FILES += \
 
 # PowerShare
 PRODUCT_PACKAGES += \
-    vendor.lineage.powershare@1.0-service.default
+    vendor.lineage.powershare-service.default
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
