c783f3c97b95ddb4660fb8397b08d8392f9508a5
Bruno Martins <bgcngm@gmail.com>
2025-04-24 21:36:24 +0100

    nuwa: Switch to PowerShare AIDL
    Change-Id: I97400638493a3b9d5a3b87fa6831c9affc7c3def

--- device.mk
+++ device.mk
@@ -46,7 +46,7 @@ PRODUCT_COPY_FILES += \
 
 # PowerShare
 PRODUCT_PACKAGES += \
-    vendor.lineage.powershare@1.0-service.default
+    vendor.lineage.powershare-service.default
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
