ee13a227024aa4ad9fb5fdd4f24ad7ba8d4e6624
Michael Bestas <mkbestas@lineageos.org>
2025-04-25 03:10:20 +0300

    nuwa: Migrate powershare to soong_config_set
    Change-Id: I2489e8d95d968556a854d7b8314f098d37c382b7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,9 +30,6 @@ TARGET_SCREEN_DENSITY := 560
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := nuwa
 
-# Powershare
-TARGET_POWERSHARE_PATH := /sys/class/qcom-battery/reverse_chg_mode
-
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/properties/odm.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
--- device.mk
+++ device.mk
@@ -55,6 +55,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.powershare-service.default
 
+$(call soong_config_set,lineage_powershare,powershare_path,/sys/class/qcom-battery/reverse_chg_mode)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
