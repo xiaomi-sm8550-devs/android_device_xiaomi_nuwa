803e25a040287c45d9f43af920fcb453d705e3b4
SkyMinus <minus_rav@qq.com>
2025-05-08 14:49:05 +0800

    nuwa: Move WifiOverlay to sm8550-common
    Signed-off-by: SkyMinus <minus_rav@qq.com>

--- device.mk
+++ device.mk
@@ -35,8 +35,7 @@ PRODUCT_PACKAGES += \
     ApertureOverlayNuwa \
     FrameworkResOverlayNuwa \
     SettingsOverlayNuwa \
-    SystemUIOverlayNuwa \
-    WifiResNuwa
+    SystemUIOverlayNuwa
 
 # Sensors
 PRODUCT_PACKAGES += \
