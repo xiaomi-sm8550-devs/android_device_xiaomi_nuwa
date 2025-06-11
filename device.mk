45f38175f703d3733aab23119aef40f44336ec08
SkyMinus <minus_rav@qq.com>
2025-06-11 06:45:12 +0000

    nuwa: Import device audio configs * Import mixer_paths from AUDIO.LA.8.0.r1-12100-KAILUA.0 * Import resourcemanager_kalama_mtp.xml from stock
    Change-Id: I74d4fb67a801ec0cf494813b511c7b372b4d9be3
    Signed-off-by: SkyMinus <minus_rav@qq.com>

--- device.mk
+++ device.mk
@@ -7,6 +7,11 @@
 # Inherit from xiaomi sm8550-common
 $(call inherit-product, device/xiaomi/sm8550-common/common.mk)
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/mixer_paths_kalama_mtp.xml \
+    $(LOCAL_PATH)/audio/resourcemanager_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/resourcemanager_kalama_mtp.xml
+
 # Init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/init.nuwa.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.nuwa.rc \
