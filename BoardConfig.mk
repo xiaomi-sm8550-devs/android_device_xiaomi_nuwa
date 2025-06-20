242806a4a168ce5ba90394784ca476b06f3f0bc7
lolipuru <banstellazz@gmail.com>
2025-06-20 02:24:28 +0000

    nuwa: Commonized device specific modules
    Change-Id: I18c7f2436d58ae77cd8d43533a4281fe4d9f9bb2
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,12 @@ include device/xiaomi/sm8550-common/BoardConfigCommon.mk
 # Display
 TARGET_SCREEN_DENSITY := 560
 
+# Kernel
+BOARD_VENDOR_KERNEL_MODULES_LOAD += \
+	cs35l41_dlkm.ko \
+	goodix_fod.ko \
+	fts_touch_spi.ko
+	
 # Inherit from the proprietary version
 include vendor/xiaomi/nuwa/BoardConfigVendor.mk
 
