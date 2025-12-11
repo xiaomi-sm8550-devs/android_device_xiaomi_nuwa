67cefe135cec45d94b5ce2e5ed5e3768fa68ebb2
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 13:30:36 +0200

    nuwa: Patch some blobs to depend on libtinyxml2-v34.so
    libtinyxml2 10.1.0 update which landed in BP4A contains ABI
    incompatible changes which cause runtime crashes.
    
    Change-Id: Icc67da5f754b0bb30dce6192b1e520fdf1349ac5

--- extract-files.py
+++ extract-files.py
@@ -59,6 +59,12 @@ blob_fixups: blob_fixups_user_type = {
     'odm/lib64/hw/camera.xiaomi.so': blob_fixup()
         .add_needed('libprocessgroup_shim.so')
         .replace_needed('libui.so', 'libui-v34.so'),
+    (
+        'odm/bin/hw/vendor.qti.camera.provider-service_64',
+        'odm/lib64/camx.provider-impl.so',
+        'odm/lib64/camera/plugins/com.xiaomi.plugin.mialgosnsc.so',
+        'odm/lib64/com.qti.feature2.anchorsync.so'
+    ): blob_fixup().replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
 }
 
 module = ExtractUtilsModule(
